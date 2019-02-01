dd571e6a926b9e09de7423d690b8953fa09827b2
Jitan <jitan.puri@tokopedia.com>
2019-02-01 23:17:51 +0530

    v2 added

--- core/conf_const.go
+++ core/conf_const.go
@@ -7,7 +7,7 @@ const (
 	PAYMENT_STAGING        = "http://localhost:5000"
 	PAYMENT_LOCAL          = "http://122.160.30.50:5092"
 	FIREBASE_KEY           = ""
-	RIDE_REQUEST_TIME      = 30 * 1000000000 * time.Nanosecond
+	RIDE_REQUEST_TIME      = 90 * 1000000000 * time.Nanosecond
 	FIREBASE_TIMEOUT       = 10
 	DRIVER_ARRIVED         = 0.100
 	DRIVER_ARRIVAL         = 0.500
