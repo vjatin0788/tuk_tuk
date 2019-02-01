1ad7fb88b221201a75a5b28f198546335f2b9cc2
Jitan <jitan.puri@tokopedia.com>
2019-02-01 13:34:21 +0530

    notif changes

--- firebase/fcm.push_notif.go
+++ firebase/fcm.push_notif.go
@@ -2,6 +2,7 @@ package firebase
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 
 	fcm "github.com/appleboy/go-fcm"
@@ -13,12 +14,21 @@ func (fb *FireBase) SendPushNotification(ctx context.Context, data interface{}, 
 
 	client := fb.FBaseClient
 
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		log.Println("[SendPushNotification][Error] Err in marshaling", err)
+		return err
+	}
+
 	msg := &fcm.Message{
 		To: deviceId,
 		Data: map[string]interface{}{
 			"response": data,
 		},
 		TimeToLive: fb.Timeout,
+		Notification: &fcm.Notification{
+			Body: string(bytes),
+		},
 	}
 
 	log.Printf("[SendPushNotification]Sending Push Notif:%+v data:%+v", data)
